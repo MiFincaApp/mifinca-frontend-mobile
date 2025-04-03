8127d1cc8fa151cf8c693182f8c0dc5610400f9e
Freyman <90077578+FreymanMc@users.noreply.github.com>
2025-04-03 17:35:34 -0500

    Se actualizo el header para que tenga funcion y se acomodo el layout (#37)

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,17 +1,28 @@
 import React from "react";
-import { View, Text, Image, StyleSheet } from "react-native";
+import { View, Text, Image, StyleSheet,Pressable,} from "react-native";
+import { useRouter } from 'expo-router'; // importamos el expo router
 
 const icon = require("@/assets/images/logo.png");
 
-const Header = () => {
+const header = () => {
+  const router = useRouter(); // usamos el router de expo
   return (
     <View style={styles.header}>
+
+      <Pressable onPress={() => router.push('/')}>
       <Image source={icon} style={styles.logo} />
+      </Pressable>
+      
       <View style={styles.headerRight}>
+      <Pressable onPress={() => router.push('/nosotros')}>
         <Text style={styles.headerText}>Obten nuestra app</Text>
+      </Pressable>
+      <Pressable onPress={() => router.push('/estadopedido')}>
         <Text style={styles.headerText}>Mis pedidos</Text>
+      </Pressable>
         <Text style={styles.headerText}>Carrito</Text>
         <Text style={styles.headerText}>Cuenta</Text>
+        
       </View>
     </View>
   );
@@ -46,7 +57,7 @@ const styles = StyleSheet.create({
   },
 });
 
-export default Header;
+export default header;
 
 
 //Se creo un componente header para optimizar el codigo de la app
